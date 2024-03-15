382c5643e0d970f5ba851ba7867f089b15e751a0
vishalk1105 <vishalvkamble95@gmail.com>
2024-03-16 05:18:43 +0530

    Sorting part1

---
+++ sorting/inertionSort.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i <= n - 1; i++)
+    {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j])
+        {
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+
+    for (int k = 0; k < n; k++)
+    {
+        cout << arr[k] << " ";
+    }
+}
